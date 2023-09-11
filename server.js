44e75b72db23442ee70909f2af46eaef21415197
1x55 <k4i@pm.me>
2023-09-11 14:43:57 +0900

    defined two routes for handling imcoming HTTP GET request

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const PORT = 8001;
+
+app.use(cors())
+
+const aliens = {}
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname, '/index.html')
+})
+
+app.get('/api/:alienName', (req,res) => {
+    const aliensName = request.params.alienName.toLowercase()
+})
